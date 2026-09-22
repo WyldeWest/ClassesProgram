429b8719f1c2a3b2ab329ca519d6d1cde9dc37ec
Marshall <wes25016@byui.edu>
2026-09-22 14:41:42 -0600

    completed program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade;
+        string gradeLetter = "";
+        Console.WriteLine("What is your grade percentage? ");
+        grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        if (percentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else if (percentage < 60)
+        {
+            gradeLetter = "F";
+        }
+        else
+        {
+            Console.WriteLine("That is not an acceptable percentage");
+        }
+        Console.WriteLine($"Your grade is {gradeLetter}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Hooray! You are on track to pass");
+        }
+        else
+        {
+            Console.WriteLine("Not looking so good. Time to study harder");
+        }
     }
 }
